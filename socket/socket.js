bf1ba6f590158d4d7a7c628aa6d38c83061e0a19
Ahmed8eith <madomama959@gmail.com>
2025-02-28 23:03:23 +0200

    cors

--- socket/socket.js
+++ socket/socket.js
@@ -1,39 +1,39 @@
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 import http from 'http'
 import express from 'express'
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    // Allow your client domain (and any other valid domain if needed)
+    origin: [
+      "https://chat-app52684.netlify.app",
+      "https://67c21ea5d6604c5baa98cc8a--chat-app52684.netlify.app"
+    ],
+    methods: ['GET', 'POST']
+  }
+});
 
-const server=http.createServer(app)
-const io=new Server(server,{
-    cors:{
-        origin:['https://mern-chat-app-backend-production-d01b.up.railway.app'],
-        methods:['GET','POST']
+export const getReciverSocketId = (reciverId) => {
+    return userSocketMap[reciverId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
+io.on('connection', (socket) => {
+    console.log("a user connected, ", socket.id);
+    const userId = socket.handshake.auth.userId;
+    if (userId !== "undefined") {
+        userSocketMap[userId] = socket.id;
     }
-})
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-export const getReciverSocketId=(reciverId)=>{
-    return userSocketMap[reciverId]
-}
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
 
-const userSocketMap={}; //{userId:socketId}
-
-io.on('connection',(socket)=>{
-    console.log("a user connected, ",socket.id)
-
-    const userId=socket.handshake.auth.userId
-    if(userId!="undefined"){
-        userSocketMap[userId]=socket.id
-    }
-
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
-
-    socket.on("disconnect", ()=>{
-        console.log("`user disconnected",socket.id)
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
-    })
-})
-
-
-export {app,io,server}
+export { app, io, server };
