d5ad505b79c6af0769562ed04882708b9a88e84d
Ahmed8eith <madomama959@gmail.com>
2025-02-28 21:43:45 +0200

    fixed

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URl,)
+        await mongoose.connect(process.env.MONGO_URL,)
         console.log("connected to mongodb!")
     } catch (error) {
         console.log("Error connecting to mongoDV", error)
