a8e95fcd67f13ab147e318571a2b1c1808e560cb
Ahmed8eith <madomama959@gmail.com>
2025-02-28 22:00:47 +0200

    cors error fixed

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,8 +8,8 @@ const generatetokenAndsetCookie=(userId,res)=>{
     res.cookie("token", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "lax", 
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "none", 
+        secure: true
       });
       
 }
